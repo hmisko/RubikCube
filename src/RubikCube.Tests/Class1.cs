3677af0aa60b5c214b712ce7d218c8df508ad658
hmisko <hmisko@gmail.com>
2017-06-12 22:27:08 +0200

    recreate

---
+++ src/RubikCube.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubikCube.Tests
+{
+    public class Class1
+    {
+    }
+}
